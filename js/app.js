9d178a07870eae94c401c9f8d6ff2a63b725f862
lirbja <lirbja@gmail.com>
2014-03-27 16:51:48 -0400

    changes to app.js

--- js/app.js
+++ js/app.js
@@ -18,27 +18,33 @@ $(document).ready(function(){
   		function randomNumber(min, max) {
   			return Math.floor(Math.random()*(max-min+1)+min);
   		}
-  		var sysNumber = (randomNumber(1, 100));
+  		// var sysNumber = (randomNumber(1, 100));
+      var sysNumber = 100; //for testing - to be removed
+
+
   		//gets userNumber
   		var userNumber = $('#userGuess').val();
+      //finds diff
+      var diff = (userNumber - sysNumber)
   		//compares userNumber to sysNumber
-  		if () {
-
+  		if (diff === 0) {
+          alert('BINGO!');
   		}
-  		else if () {
-
+      // NEED TO DEBUG THIS STUFF!!!
+      // else if ((1 >= diff && diff <= 10) || (-1 <= diff && diff >=- 10)) {
+      //     alert('Very Hot');
+      // }
+      else if ((11 >= diff && diff <= 20) || (-11 <= diff && diff >=-20)) {
+          alert('Hot');
+      }   
+      else if ((21 >= diff && diff <= 30) || (-21 <= diff && diff >=-30)) {
+          alert('Warm');
+      }                     
+  		else if ((31 >= diff && diff <= 50) || (-31 <= diff && diff >=-50)) {
+          alert('Cold');
   		}
-  		else if () {
-
-  		}
-  		else if () {
-
-  		}
-  		else if () {
-
-  		}
-  		else {
-
+  		else if (diff > 50 || diff < -50) {
+          alert('Ice Cold');
   		}
   		//appends user guess to #guesslist
 
