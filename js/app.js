176844f6abac556222ea0c1c13bc3634fddc3e97
lirbja <lirbja@gmail.com>
2014-03-27 16:19:55 -0400

    initial commit

--- js/app.js
+++ js/app.js
@@ -12,6 +12,39 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
+  	//on click, game starts
+  	$('form').on('click', '#guessButton', function() {
+  		//generates random number
+  		function randomNumber(min, max) {
+  			return Math.floor(Math.random()*(max-min+1)+min);
+  		}
+  		var sysNumber = (randomNumber(1, 100));
+  		//gets userNumber
+  		var userNumber = $('#userGuess').val();
+  		//compares userNumber to sysNumber
+  		if () {
+
+  		}
+  		else if () {
+
+  		}
+  		else if () {
+
+  		}
+  		else if () {
+
+  		}
+  		else if () {
+
+  		}
+  		else {
+
+  		}
+  		//appends user guess to #guesslist
+
+  		//counts user guesses and logs to span#count
+
+  	});
+
+
 });
-
-
